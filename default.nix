3ba5e0c2d1a1757f2d0c9f95aec235dc5bd1c901
3JlOy_PYCCKUI <3jl0y_pycckui@riseup.net>
2026-08-17 18:31:41 +0300

    another approach

--- default.nix
+++ default.nix
@@ -2,10 +2,4 @@ let
   nixpkgs = builtins.getFlake "git+https://github.com/nixos/nixpkgs?ref=nixos-unstable&shallow=1";
   pkgs = import nixpkgs { system = builtins.currentSystem; };
 in
-pkgs.runCommand "test" {
-  nativeBuildInputs = with pkgs; [ pkg-config ];
-  buildInputs = with pkgs; [ mpv ];
-}
-''
-  echo \#; pkg-config --list-all; echo \#; # exit 1
-''
+pkgs.callPackage ./pkg {}
